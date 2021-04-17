ff18f35db66b10b4cfdaee99d4358febc0bc5f08
gyoritamas <gyoritamasnoe@gmail.com>
2021-04-17 20:41:16 +0200

    1-3 feladat kész

---
+++ Operatorok/ArithmeticExpression.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorok
+{
+    public class ArithmeticExpression
+    {
+        public int FirstOperand { get; set; }
+        public string Operaator { get; set; }
+        public int SecondOperand { get; set; }
+
+        public ArithmeticExpression(int firstOperand, string operaator, int secondOperand)
+        {
+            FirstOperand = firstOperand;
+            Operaator = operaator;
+            SecondOperand = secondOperand;
+        }
+    }
+}
--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -3,13 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Operatorok
 {
     class Program
     {
+        const string FILE_PATH = "src\\kifejezesek.txt";
+        static List<ArithmeticExpression> arithmeticExpressions;
         static void Main(string[] args)
         {
+            #region 1. feladat
+            arithmeticExpressions = ReadSourceFile(FILE_PATH);
+            #endregion
+
+            #region 2. feladat
+            var totalNumberOfExpressions = arithmeticExpressions.Count();
+            Console.WriteLine($"2. feladat: Kifejezések száma: {totalNumberOfExpressions}");
+            #endregion
+
+            #region 3. feladat
+            var numberOfExpressionsWithMod = arithmeticExpressions
+                .Select(x => x)
+                .Where(x => x.Operaator.Equals("mod"))
+                .Count();
+
+            Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {numberOfExpressionsWithMod}");
+            #endregion
+            Console.ReadKey();
+
+        }
+
+        private static List<ArithmeticExpression> ReadSourceFile(string filePath)
+        {
+            List<ArithmeticExpression> result = new List<ArithmeticExpression>();
+
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] arithmethicExpressionString = line.Split(' ');
+
+                Int32.TryParse(arithmethicExpressionString[0], out int firstOperand);
+                string operaator = arithmethicExpressionString[1];
+                Int32.TryParse(arithmethicExpressionString[0], out int secondOperand);
+
+                result.Add(new ArithmeticExpression(firstOperand, operaator, secondOperand));
+
+            }
+
+            return result;
         }
     }
 }
